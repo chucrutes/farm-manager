1ee04c30141c753db68e4c98b6efaee526271b4c
chucrutes <tmelo387@gmail.com>
2024-03-09 18:27:38 -0300

    fix: deleting page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,17 +1,7 @@
-import AddItemForm from "@/components/organisms/forms/AddItemForm";
-import { useState } from "react";
-import { IItem } from "../entitities/item.schema";
-
 const DashboardPage = () => {
-  const [items, setItems] = useState<IItem[]>([]);
-
-  const saveItem = (item: IItem) => {
-    setItems((prev) => [item, ...prev]);
-  };
-
   return (
     <div className="flex flex-col items-center">
-      <AddItemForm saveItem={saveItem} />
+      <p>Dashboard</p>
     </div>
   );
 };
