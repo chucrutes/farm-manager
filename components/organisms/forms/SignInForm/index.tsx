dc80e106f17c1fdf4989a4cd0f8bf7e46cdcc259
chucrutes <tmelo387@gmail.com>
2024-03-23 16:50:22 -0300

    fix: minor changes

--- components/organisms/forms/SignInForm/index.tsx
+++ components/organisms/forms/SignInForm/index.tsx
@@ -5,11 +5,11 @@ import { useForm } from "react-hook-form";
 import Alert from "@/components/atoms/Span";
 import Label from "@/components/atoms/Label";
 import Input from "@/components/atoms/Input";
-import { Button } from "@/components/atoms/Button";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ErrorIcon } from "@/components/Icons/ErrorIcon";
 import { IUser, signInSchema } from "@/app/entities/IUser";
 import LabeledInput from "@/components/molecules/LabeledInput";
+import Button from "@/components/atoms/Button";
 
 const SignInForm = () => {
 	const {
@@ -36,12 +36,24 @@ const SignInForm = () => {
 					<LabeledInput>
 						<Label htmlFor={userTagId}>Usuário</Label>
 						<Input label="Usuário" id={userTagId} {...register("user")} />
-            {errors.user && <Alert icon={<ErrorIcon/>} severity="error" />}
+						{errors.user && (
+							<Alert icon={<ErrorIcon />} severity="error">
+								Usuário inválido
+							</Alert>
+						)}
 					</LabeledInput>
 					<LabeledInput>
 						<Label htmlFor={passwordTagId}>Senha</Label>
-						<Input label="Usuário" id={passwordTagId} {...register("password")} />
-            {errors.password && <Alert icon={<ErrorIcon/>} severity="error" />}
+						<Input
+							label="Usuário"
+							id={passwordTagId}
+							{...register("password")}
+						/>
+						{errors.password && (
+							<Alert icon={<ErrorIcon />} severity="error">
+								Senha inválida
+							</Alert>
+						)}
 					</LabeledInput>
 					<div>
 						<Button type="submit">Enviar</Button>
