d85ed56c97de61f156de07ebf3531e26ed864226
chucrutes <tmelo387@gmail.com>
2024-03-23 16:45:53 -0300

    fix: minor changes

--- components/atoms/Select.tsx
+++ components/atoms/Select.tsx
@@ -13,27 +13,25 @@ type SelectProps = {
 	options: SelectOption[];
 };
 
-const Select = forwardRef(
-	(props: SelectProps, ref: ForwardedRef<HTMLSelectElement>) => {
-		const { options } = props;
-		const [option, setOption] = useState<SelectOption>(options[0]);
+const Select = forwardRef<HTMLSelectElement, SelectProps>((props, ref) => {
+	const { options } = props;
+	const [option, setOption] = useState<SelectOption>(options[0]);
 
-		const onChange = ({ target: { value } }: SelectChangeEvent) => {
-			const option = options.find((option) => option.value === value)!;
-			setOption(option);
-		};
+	const onChange = ({ target: { value } }: SelectChangeEvent) => {
+		const option = options.find((option) => option.value === value)!;
+		setOption(option);
+	};
 
-		return (
-			<MuiSelect ref={ref} value={option.value} onChange={onChange}>
-				{options.map((option) => (
-					<MenuItem key={option.value} value={option.value}>
-						{option.label}
-					</MenuItem>
-				))}
-			</MuiSelect>
-		);
-	},
-);
+	return (
+		<MuiSelect ref={ref} value={option.value} onChange={onChange}>
+			{options.map((option) => (
+				<MenuItem key={option.value} value={option.value}>
+					{option.label}
+				</MenuItem>
+			))}
+		</MuiSelect>
+	);
+});
 
 Select.displayName = "Select";
 
