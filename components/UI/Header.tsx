a82e8f69114d23db2288c93c0c71a51a1b4a2fd6
chucrutes <tmelo387@gmail.com>
2024-04-03 10:39:43 -0300

    fix: removing unused style

--- components/UI/Header.tsx
+++ components/UI/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
 	return (
 		<header className="flex flex-col md:flex-row justify-around items-center pt-4 px-4 relative bottom-0 bg-brown-dark">
 			<h1 className="text-center md:text-left">Gerenciador de fazenda</h1>
-			<nav className="px-4 md:px-32 mt-4 md:mt-0 align-middle">
+			<nav className="px-4 md:px-32 mt-4 md:mt-0">
 				<ul className="flex flex-row justify-center md:justify-end">
 					<LinkItem path="/">
 						<LiItem content="Início" />
