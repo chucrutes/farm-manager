c3c383896f29a94eff40481c24567bc7c8bd38f0
Thiago Melo <tmelo387@gmail.com>
2025-03-04 09:58:10 -0300

    fix: changing input to be controlled

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -2,12 +2,10 @@ import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import type { TextFieldProps } from "@mui/material";
 import { Eye, EyeOff } from "lucide-react";
-import { useFormContext, type ValidationRule } from "react-hook-form";
 
 type InputProps = TextFieldProps & {
   label?: string;
   id: string;
-  pattern?: ValidationRule<RegExp>;
 };
 
 function Input({
@@ -16,21 +14,19 @@ function Input({
   type = "text",
   defaultValue,
   ref,
+  value,
   ...props
 }: InputProps) {
-  const { register, watch } = useFormContext();
   const [isVisible, setIsVisible] = useState(type !== "password");
   const togglePassword = () => {
     setIsVisible((prev) => !prev);
   };
 
-  const value = watch(id);
   return (
-    <div>
+    <div className="relative w-full">
       <TextField
-        defaultValue={defaultValue}
+        value={value}
         {...props}
-        {...register(id)}
         InputLabelProps={{ shrink: !!value }}
         label={label}
         type={isVisible ? "text" : "password"}
@@ -43,19 +39,28 @@ function Input({
             fontFamily: "Sora, sans-serif",
             fontSize: "13px",
             padding: "6px 14px",
+            paddingRight: "40px",
           },
           "& label": {
             fontFamily: "Sora, sans-serif",
             fontSize: "14px",
           },
           width: "100%",
+          position: "relative",
         }}
       />
       {type === "password" && (
         <button
           type="button"
           onClick={togglePassword}
-          className="absolute right-2 top-1/2 transform -translate-y-1/5 cursor-pointer"
+          className="absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer"
+          style={{
+            position: "absolute",
+            right: "10px",
+            top: "50%",
+            transform: "translateY(-50%)",
+            zIndex: 10,
+          }}
         >
           {isVisible ? (
             <Eye className="size-4 text-gray-500" />
