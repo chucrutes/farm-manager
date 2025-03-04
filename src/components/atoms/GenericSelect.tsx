648e96d9fe384bfa2871d03181840f5b9c56274e
Thiago Melo <tmelo387@gmail.com>
2025-03-04 09:57:39 -0300

    fix: changing select to be controlled

--- src/components/atoms/GenericSelect.tsx
+++ src/components/atoms/GenericSelect.tsx
@@ -1,73 +1,48 @@
-import {
-  Controller,
-  useFormContext,
-  type RegisterOptions,
-} from "react-hook-form";
+import { ComponentProps } from "react";
 
 export type Option = {
   label: string;
   value: string;
 };
 
-type GenericSelectProps = {
-  name: string;
+export type SelectProps = ComponentProps<"select"> & {
   options: Option[];
-  label?: string;
-  rules?: RegisterOptions;
+  label: string;
   placeholder?: string;
-  value?: string;
 };
 
 const GenericSelect = ({
   name,
-  options,
   label,
-  rules,
-  value,
   placeholder,
-}: GenericSelectProps) => {
-  const { control } = useFormContext();
-
+  options,
+  onChange,
+}: SelectProps) => {
   return (
     <div className="form-group">
-      {label && (
-        <label htmlFor={name} className="block mb-2 text-sm font-medium">
-          {label}
-        </label>
-      )}
-      <Controller
-        name={name}
-        control={control}
-        defaultValue={value || ""}
-        rules={rules}
-        render={({ field, fieldState: { error } }) => (
-          <div className="relative">
-            <select
-              id={name}
-              {...field}
-              className={`form-select w-full p-2 border rounded-md ${
-                error ? "border-red-500" : "border-gray-300"
-              } focus:outline-none focus:ring-2 focus:ring-blue-500`}
-            >
-              {placeholder && (
-                <option value="" disabled>
-                  {placeholder}
-                </option>
-              )}
-              {options.map((option) => (
-                <option key={String(option.value)} value={String(option.value)}>
-                  {option.label}
-                </option>
-              ))}
-            </select>
-            {error && (
-              <span className="text-red-500 text-sm mt-1 block">
-                {error.message}
-              </span>
-            )}
-          </div>
-        )}
-      />
+      <label htmlFor={name} className="block mb-2 text-sm font-medium">
+        {label}
+      </label>
+
+      <div className="relative">
+        <select
+          id={name}
+          onChange={onChange}
+          className={`form-select w-full p-2 border rounded-md
+           border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500`}
+        >
+          {
+            <option value="" disabled>
+              {placeholder}
+            </option>
+          }
+          {options.map((option) => (
+            <option key={String(option.value)} value={String(option.value)}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 };
