8d9c65a89d79c1f8f544d4571cbbe97370822b1f
Thiago <tmelo387@gmail.com>
2025-03-12 18:23:04 -0300

    fix: add absolute to return negative numbers

--- src/@utils/formatters.ts
+++ src/@utils/formatters.ts
@@ -6,8 +6,8 @@ export const stringToNumber = (value: string) => {
 };
 
 export const formatBrazilianCurrency = (amount: number) => {
-return new Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
-  }).format(amount);
-}
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(Math.abs(amount));
+};
