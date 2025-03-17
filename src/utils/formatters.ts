40c5b5d12e945866bd6f8975d77887b1d342f069
Lucas Carvalho <lucasvierac@gmail.com>
2025-03-17 13:46:06 -0300

    fix: change format date

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -2,6 +2,7 @@ export const brDateFormatter = (date: Date) => {
   return date.toLocaleDateString("pt-BR", {
     day: "2-digit",
     month: "2-digit",
+    year: "2-digit",
     hour: "2-digit",
     minute: "2-digit",
   });
