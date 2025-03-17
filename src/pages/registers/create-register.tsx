1bb3d2cae2b00c9dce70fa574d52364f1a665460
Lucas Carvalho <lucasvierac@gmail.com>
2025-03-17 17:12:28 -0300

    feat: add create register

---
+++ src/pages/registers/create-register.tsx
@@ -0,0 +1,12 @@
+import AuthLayout from "../layout/auth-layout";
+import { CreateRegisterComponent } from "../../components/organisms/create-register.page";
+
+const CreateRegistersPage = () => {
+  return (
+    <AuthLayout>
+      <CreateRegisterComponent />
+    </AuthLayout>
+  );
+};
+
+export default CreateRegistersPage;
