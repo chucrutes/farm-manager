d46de2f714d0c0c95c712b1327ddc09000e5c400
Lucas Carvalho <lucasvierac@gmail.com>
2025-03-17 17:12:57 -0300

    feat: add new routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import DashboardPage from "./pages/dashboard";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RootLayout from "./pages/layout/root-layout";
 import TypesPage from "./pages/types";
+import RegistersPage from "./pages/registers";
+import CreateRegistersPage from "./pages/registers/create-register";
+import RegisterDetailPage from "./pages/registers/[id]";
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
           <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/" element={<Home />} />
           <Route path="/types" element={<TypesPage />} />
+          <Route path="/registers" element={<RegistersPage />} />
+          <Route path="/register/new" element={<CreateRegistersPage />} />
+          <Route path="/register/:id" element={<RegisterDetailPage />} />
         </Routes>
       </Router>
     </RootLayout>
