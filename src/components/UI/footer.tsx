ced43506913ce152031bbac7a80af6069ac3d740
Thiago <tmelo387@gmail.com>
2025-04-16 21:03:03 -0300

    fix: minor changes

--- src/components/UI/footer.tsx
+++ src/components/UI/footer.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
-  return (
-    <footer className="flex justify-center">
-      &copy; 2024 por Thiago Melo. Direitos reservados.
-    </footer>
-  );
+	return (
+		<footer className="flex justify-center">
+			&copy; 2024 por Lazy Labs. Direitos reservados.
+		</footer>
+	);
 };
 
 export default Footer;
